cb416998a3480004324859f93e5d5f1f9f88ef4d
Noam Koren <noamko25@gmail.com>
2021-12-21 15:15:12 +0200

    Added RGB pinout defs

---
+++ include/RGBHandler.h
@@ -0,0 +1,5 @@
+#ifndef RGB_HANDALER_H
+#define RGB_HANDALER_H
+
+void init()
+#endif
---
+++ include/pinout.h
@@ -0,0 +1,49 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+//RGB pinout
+#define RGB_R_A 44
+#define RGB_G_A 45
+#define RGB_B_A 42
+
+#define RGB_R_B 43
+#define RGB_G_B 40
+#define RGB_B_B 41
+
+#define RGB_R_C 38
+#define RGB_G_C 39
+#define RGB_B_C 36
+
+#define RGB_R_D 37
+#define RGB_G_D 34
+#define RGB_B_D 35
+
+#define RGB_R_D 32
+#define RGB_G_D 33
+#define RGB_B_D 30
+
+#define RGB_R_D 31
+#define RGB_G_D 28
+#define RGB_B_D 29
+
+#define RGB_R_D 31
+#define RGB_G_D 28
+#define RGB_B_D 29
+
+#define RGB_R_D 26
+#define RGB_G_D 27
+#define RGB_B_D 24
+
+#define RGB_R_D 25
+#define RGB_G_D 22
+#define RGB_B_D 23
+
+#define RGB_COM_8 47
+#define RGB_COM_7 46
+#define RGB_COM_6 49
+#define RGB_COM_5 48
+#define RGB_COM_4 51
+#define RGB_COM_3 50
+#define RGB_COM_2 53
+#define RGB_COM_1 52
+#endif
